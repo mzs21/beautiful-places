012fc5a3fdac0f427d6e42649ba29594366ef7b1
mzs21 <snigdho2111@gmail.com>
2021-07-17 17:47:15 +0600

    script file updated

--- script.ts
+++ script.ts
@@ -1,6 +1,13 @@
 let bodyTag = document.querySelector('body') as HTMLBodyElement;
 
+
+
 bodyTag.addEventListener('mousemove', (e:MouseEvent) =>
 {
-    bodyTag.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)";
+    let color1 = "rgb("+e.offsetX+","+e.offsetY+", 40)";
+    let color2 = "rgb("+e.offsetY+","+e.offsetX+", 40)";
+
+    bodyTag.style.backgroundImage = 'linear-gradient(100deg,'+ color1+',red,orange,yellow, '+ color2+', green,blue,indigo,violet)';
+
+    // bodyTag.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)";
 });
--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
 "use strict";
 var bodyTag = document.querySelector('body');
 bodyTag.addEventListener('mousemove', function (e) {
-    bodyTag.style.backgroundColor = "rgb(" + e.offsetX + "," + e.offsetY + ", 40)";
+    var color1 = "rgb(" + e.offsetX + "," + e.offsetY + ", 40)";
+    var color2 = "rgb(" + e.offsetY + "," + e.offsetX + ", 40)";
+    bodyTag.style.backgroundImage = 'linear-gradient(100deg,' + color1 + ',red,orange,yellow, ' + color2 + ', green,blue,indigo,violet)';
+    // bodyTag.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)";
 });
